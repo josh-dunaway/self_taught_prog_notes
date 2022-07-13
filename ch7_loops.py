b78e8599a9e3d04cb237fd210a26eaa35a87080f
Joshua Dunaway <joshuadunaway23@gmail.com>
2022-07-13 17:51:41 +0200

    starting ch 7

---
+++ ch7_loops.py
@@ -0,0 +1,8 @@
+print('''I already have quite a bit of experience with loops in other programming
+languages. I will give basic examples but will mainly only note those things I find
+interesting.''')
+
+print('''
+Iterables that can be iterated with for loop:
+- Sequences: lists, tuples, and strings
+- sequences can be''')
