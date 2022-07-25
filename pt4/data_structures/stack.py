61d4a345bb41c5c613585ef14eee786ca21a8abd
Joshua Dunaway <joshuadunaway23@gmail.com>
2022-07-25 12:53:44 +0200

    Implemented Stack

---
+++ pt4/data_structures/stack.py
@@ -0,0 +1,36 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
+
+#simple example use case for Stack
+def reverse_string(string):
+    stack = Stack()
+    try:
+        for c in string:
+            stack.push(c)
+    except TypeError:
+        return
+    reverse = ''
+    while(not stack.is_empty()):
+        reverse += stack.pop()
+    return reverse
+
+#testing Stack using reverse_string function
+stack2 = Stack()
+print(reverse_string('abcdefghijklmnopqrstuvwxyz'))
+print(reverse_string(stack2))
